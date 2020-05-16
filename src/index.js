c80389afac3a71ada84e26af63e83f61a47a49a9
m <mbe_pro@outlook.com>
2020-05-16 14:03:46 -0700

    draft of list fn

--- src/index.js
+++ src/index.js
@@ -11,73 +11,92 @@ const app = express();
 const client = new Discord.Client();
 
 client.on("ready", () => {
-	
-	console.log("Ready!");
+  console.log("Ready!");
 
-	for (const guild of client.guilds.cache.array()) {
-		random_garbage = guild.channels.cache.array().find(_ => _.name == "random-garbage");
-	}
-
+  for (const guild of client.guilds.cache.array()) {
+    random_garbage = guild.channels.cache
+      .array()
+      .find((_) => _.name == "random-garbage");
+  }
 });
 
-function chunks (buffer, chunkSize) {
-	assert(Buffer.isBuffer(buffer),           'Buffer is required');
-	assert(!isNaN(chunkSize) && chunkSize > 0, 'Chunk size should be positive number');
+function chunks(buffer, chunkSize) {
+  assert(Buffer.isBuffer(buffer), "Buffer is required");
+  assert(
+    !isNaN(chunkSize) && chunkSize > 0,
+    "Chunk size should be positive number"
+  );
 
-	var result = [];
-	var len = buffer.length;
-	var i = 0;
+  var result = [];
+  var len = buffer.length;
+  var i = 0;
 
-	while (i < len) {
-		result.push(buffer.slice(i, i += chunkSize));
-	}
+  while (i < len) {
+    result.push(buffer.slice(i, (i += chunkSize)));
+  }
 
-	return result;
+  return result;
 }
 
-function assert (cond, err) {
-	if (!cond) {
-		throw new Error(err);
-	}
+function assert(cond, err) {
+  if (!cond) {
+    throw new Error(err);
+  }
 }
 
-function uploadBuffer (channel, name, buffer) {
-	let i = 0;
-	const chonks = chunks(buffer, 8388119);
-	const random = Math.random().toString(36).replace("0.", "");
+function uploadBuffer(channel, name, buffer) {
+  let i = 0;
+  const chonks = chunks(buffer, 8388119);
+  const random = Math.random().toString(36).replace("0.", "");
 
-	for (const chunk of chonks) {
-		channel.send(`${name}_${random}, part ${i + 1}`, {
-			files: [
-				chunk
-			]
-		});
+  for (const chunk of chonks) {
+    channel.send(`${name}_${random}, part ${i + 1}`, {
+      files: [chunk],
+    });
 
-		i++;
-	}
+    i++;
+  }
 }
 
-client.on("message", async message => {
+client.on("message", async (message) => {
+  const { content, channel } = message;
 
-	if (message.content === "/delete") {
-		const messages = (await message.channel.messages.fetch()).array();
+  if (message.content === "/delete") {
+    const messages = (await message.channel.messages.fetch()).array();
 
-		for (const cmessage of messages) {
-			if (cmessage.deletable) cmessage.delete();
-		}
+    for (const cmessage of messages) {
+      if (cmessage.deletable) cmessage.delete();
+    }
 
-		message.reply("I oblige, master.");
-	} else if (message.content === "/upload_test") {
-		const file_data = fs.readFileSync(path.join(__dirname, "..", "test", "inkscape.exe"));
-		uploadBuffer(message.channel, "UPLOAD inkscape.exe (Windows)", file_data);
-	}
+    message.reply("I oblige, master.");
+  } else if (message.content === "/upload_test") {
+    const file_data = fs.readFileSync(
+      path.join(__dirname, "..", "test", "inkscape.exe")
+    );
+    uploadBuffer(message.channel, "UPLOAD inkscape.exe (Windows)", file_data);
+  } else if (content === "/list") {
+    const messages = (await channel.messages.fetch()).array();
 
+    let names = [];
+    for (const m of messages) {
+      const { author, content } = m;
+      if (m.indexOf("-- INTERRUPT --") > -1) break;
+      // :^)
+      if (author === "DiscordDB" && content.indexOf("UPLOAD") > -1) {
+        const splitMsg = content.split("part")[1].split(",");
+        const fileName = splitMsg.split("UPLOAD ")[1];
+        const partStrings = splitMsg[1].split("/").map((s) => s.trim());
+        const partNo = partStrings[0];
+        const totalParts = partStrings[1].split(" ")[0];
+        if (partNo === totalParts) names.push(fileName);
+      }
+    }
+    message.reply(names.join(","));
+  }
 });
 
 app.get("/", (req, res) => {
-
-	res.render("upload.ejs", {});
-
+  res.render("upload.ejs", {});
 });
 
 client.login(process.env.TOKEN);
