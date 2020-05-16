d65001f5e3497def86640bd38d886c26235f09e1
j <jacketsj@gmail.com>
2020-05-16 11:50:17 -0700

    Sauce

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ const client = new Discord.Client();
 
 client.on("message", message => {
 
-	if (message.content === "dorito")
+	if (message.content === "dorito") {
 		message.reply("doritooooo");
-
+	}
+	if (message.content === "dorito?") {
+		message.reply("yus doritooooo");
+	}
+	if (message.content === "sauce") {
+		message.channel.send("Here sauce", {
+			files: [
+				'./src/index.js'
+			]
+		});
+	}
 });
 
 client.login(process.env.TOKEN);
