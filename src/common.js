eb3cb389d32b28747530c9dc72522e62be0f2c64
mebird <49777777+mebird@users.noreply.github.com>
2020-05-17 01:28:24 -0700

    Big Refactor Boi (#10)
    See readme for quick rundown.

---
+++ src/common.js
@@ -0,0 +1,168 @@
+const baseUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
+
+function chunks(buffer, chunkSize) {
+  assert(Buffer.isBuffer(buffer), "Buffer is required");
+  assert(
+    !isNaN(chunkSize) && chunkSize > 0,
+    "Chunk size should be positive number"
+  );
+
+  var result = [];
+  var len = buffer.length;
+  var i = 0;
+
+  while (i < len) {
+    result.push(buffer.slice(i, (i += chunkSize)));
+  }
+
+  return result;
+}
+
+function assert(cond, err) {
+  if (!cond) {
+    throw new Error(err);
+  }
+}
+
+/**
+ * Uploads a buffer to DisDB, splitting it into 8mb chunks and upload in parts.
+ * @param {Discord.TextChannel} channel 
+ * @param {string} name 
+ * @param {Buffer} buffer 
+ * @param {string} [uuid] 
+ * @param {number} [offset]
+ */
+function uploadBuffer(
+  channel,
+  name,
+  buffer,
+  uuid = Math.random().toString(36).replace("0.", ""),
+  offset = 0
+) {
+  let i = 0;
+  const chonks = chunks(buffer, 8388119);
+  const a = [];
+  for (const chunk of chonks) {
+    a.push(
+      channel.send(
+        `UPLOAD ${name}_${uuid}, part ${i + 1 + offset} / ${chonks.length} (${
+          chonks.length - i - 1
+        } seconds remaining)`,
+        {
+          files: [chunk],
+        }
+      )
+    );
+
+    i++;
+  }
+
+  return { ...Promise.all(a), length: a.length };
+}
+
+/**
+ * Parses the content from an "UPLOAD" DisDB message.
+ * @param {string} content 
+ * @returns {filename: string, partNo: string, totalParts: string}
+ */
+const parseMessageContent = (content) => {
+  const splitMsg = content.split(",");
+  const filename = splitMsg[0].split("UPLOAD ")[1];
+  const partStrings = splitMsg[1].split("/").map((s) => s.trim());
+  const partNo = partStrings[0].split(" ")[1].trim();
+  const totalParts = partStrings[1].split(" ")[0];
+  return {
+    filename,
+    partNo,
+    totalParts,
+  };
+};
+
+const xss = (str) =>
+  str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#x27")
+    .replace(/\//g, "&#x2F");
+
+/**
+ * Lists all valid files in a DisDB storage channel.
+ * @param {Discord.TexChannel} channel 
+ */
+const listFiles = async (channel) => {
+  let messages = (await channel.messages.fetch()).array();
+  let names = new Set();
+  let i = 0;
+  while (i < messages.length) {
+    const { author, content, id } = messages[i];
+    if (content.indexOf("-- INTERRUPT --") > -1) break;
+    if (author.bot && content.indexOf("UPLOAD") > -1) {
+      const { filename, partNo, totalParts } = parseMessageContent(content);
+      if (partNo === totalParts) names.add(filename);
+    }
+
+    i++;
+
+    if (i === messages.length) {
+      i = 0;
+      messages = (
+        await channel.messages.fetch({
+          before: id,
+        })
+      ).array();
+    }
+  }
+  return [...names].map((str) => xss(str));
+};
+
+/************************************************************************
+  Delete file(s) from the given channel.
+ ************************************************************************/
+
+ /**
+ * Deletes a file from the DisDB channel.
+ * The filename must match a <NAME>.<EXTENSION>_<UUID> format.
+ * @param {string} filename
+ * @param {Discord.TexChannel} channel 
+ */
+const deleteFile = async (reqFilename, channel) => {
+  let messages = (await channel.messages.fetch()).array();
+  console.log("Deleting file...", reqFilename);
+
+  while (messages.length) {
+    const m = messages.pop();
+    const {
+      author: { bot },
+      content,
+      deletable,
+      id,
+    } = m;
+    if (content.indexOf("-- INTERRUPT --") > -1) break;
+    if (bot && deletable && content.indexOf("UPLOAD") > -1) {
+      const { filename: msgFilename } = parseMessageContent(content);
+      if (reqFilename === msgFilename) m.delete();
+    }
+    if (!messages.length)
+      messages = (await channel.messages.fetch({ before: id })).array();
+  }
+};
+
+/**
+ * Wipes all DisDB files from a discord channel.
+ * @param {Discord.TexChannel} channel 
+ */
+const deleteAllFiles = async (channel) => {
+  const messages = (await channel.messages.fetch()).array();
+  messages.forEach((m) => m.deletable && m.delete());
+};
+
+module.exports = {
+  baseUrl,
+  uploadBuffer,
+  listFiles,
+  parseMessageContent,
+  deleteFile,
+  deleteAllFiles,
+};
---
+++ src/controllers.js
@@ -0,0 +1,105 @@
+const axios = require("axios");
+
+const { uploadBuffer, baseUrl } = require("./common");
+const { getDefaultChannel } = require("./discord");
+
+const uploadHandler = async (req, res) => {
+  if (!req.files.fileList) {
+    res.redirect("/");
+    return;
+  }
+  if (Array.isArray(req.files.fileList)) {
+    for (let file of req.files.fileList) {
+      await uploadBuffer(getDefaultChannel(), file.name, file.data);
+    }
+  } else {
+    try {
+      await uploadBuffer(
+        getDefaultChannel(),
+        req.files.fileList.name,
+        req.files.fileList.data
+      );
+    } catch {
+      // do nothing
+    }
+  }
+  res.redirect("/");
+};
+
+let downloadFn = async (req, res) => {
+  let messages = (await getDefaultChannel().messages.fetch()).array();
+  let filename = req.params.file;
+
+  let i = 0;
+  let arrthingy = [];
+  while (i < messages.length) {
+    const cmessage = messages[i];
+    if (
+      cmessage.content.startsWith("UPLOAD") &&
+      cmessage.content.includes(filename)
+    ) {
+      let partnumber = parseInt(cmessage.content.match(/\d+ \/ /g)[0]);
+      arrthingy[partnumber - 1] = axios.get(
+        cmessage.attachments.array()[0].attachment,
+        {
+          responseType: "arraybuffer",
+        }
+      );
+    }
+
+    i++;
+
+    if (i === messages.length) {
+      i = 0;
+      messages = (
+        await getDefaultChannel().messages.fetch({
+          before: cmessage.id,
+        })
+      ).array();
+    }
+  }
+
+  if (arrthingy.length === 0)
+    return res.json({
+      error: "Could not find file.",
+    });
+
+  var buf = Buffer.concat((await Promise.all(arrthingy)).map((_) => _.data));
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${encodeURIComponent(
+      filename.split("_").slice(0, -1).join("")
+    )}"`
+  );
+  res.write(buf);
+  res.end();
+};
+
+const downloadHandler = async (req, res) => {
+  try {
+    await downloadFn(req, res);
+  } catch {
+    // do nothing
+  }
+};
+
+const streamHandler = async (req, res) => {
+  /**
+   * @type {Discord.VoiceChannel}
+   */
+  const vc = getDefaultChannel()
+    .guild.channels.cache.array()
+    .filter((_) => _.type === "voice")
+    .find((_) => _.name === req.params.channel);
+
+  vc.join().then((conn) => {
+    conn.play(`${baseUrl}download/${encodeURIComponent(req.params.file)}`);
+    res.redirect("/");
+  });
+};
+
+module.exports = {
+  uploadHandler,
+  downloadHandler,
+  streamHandler,
+};
