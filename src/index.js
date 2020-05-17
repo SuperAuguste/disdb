22a0f452cc5dd06cb5a7414d78b4f5424b8a23d5
mebird <49777777+mebird@users.noreply.github.com>
2020-05-16 17:29:48 -0700

    Fixin Backslash Issue (#6)

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const Discord = require("discord.js");
 let random_garbage;
 const app = express();
 
-const baseUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
+const baseUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}/`;
 
 app.use(express.static("static"));
 app.use(require("express-fileupload")());
@@ -101,7 +101,7 @@ const linkFiles = async (channel, message) => {
 	const embedded = new Discord.MessageEmbed();
 	embedded.description = fileNames.length 
 		? fileNames
-			.map(n => `[${n.substring(0, n.lastIndexOf("_"))}](${baseUrl}/download/${encodeURIComponent(n)})`)
+			.map(n => `[${n.substring(0, n.lastIndexOf("_"))}](${baseUrl}download/${encodeURIComponent(n)})`)
 			.join("\n")
 		: "Unable to find any uploaded files!";
 	message.reply(embedded);
